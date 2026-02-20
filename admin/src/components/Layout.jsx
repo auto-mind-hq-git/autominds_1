218585a7c8b25a4f0b7ab3388606f24420ba1d65
autominds <automindhq@gmail.com>
2026-02-21 01:01:28 +0530

    fix: force page re-render on navigation with key={location.pathname}

--- admin/src/components/Layout.jsx
+++ admin/src/components/Layout.jsx
@@ -130,6 +130,7 @@ export const Topbar = ({ onMenuClick }) => {
 
 const Layout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const location = useLocation();
 
     return (
         <div className="flex h-screen bg-[#0a1628] overflow-hidden">
@@ -138,7 +139,7 @@ const Layout = () => {
             <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
                 <Topbar onMenuClick={() => setIsSidebarOpen(true)} />
                 <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8 custom-scrollbar relative">
-                    <div className="max-w-7xl mx-auto">
+                    <div className="max-w-7xl mx-auto" key={location.pathname}>
                         <Outlet />
                     </div>
                 </main>
